f1ed9f58af1de671612afd1309fd7c83420e321a
LONG <2447417474@qq.com>
2022-09-06 18:14:02 +0800

    feat:登录页完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
   <div>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// git log 查看commit记录cls
-// git status 查看当前仓库的状态
-// git add . 提交到暂存区
-// git commit -m “” （双引号里写这次存档的说明，存了啥）
-//    - feat：新增特性
-//    - style：样式的修改
-//    - fixed：debug
-//    - comment：添加了注释
 export default {}
 </script>
 
-<style scoped>
-  .box {
-    width: 2rem;
-    height: 2rem;
-    background-color: gold;
-  }
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    // 首屏加载速度很慢
+    // 设置路由懒加载
+    //   - 使用到了路由页面，再去请求他
+    component: () => import('@/views/Login')
+  }
+]
 
 const router = new VueRouter({
   routes
