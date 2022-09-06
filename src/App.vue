8355dfbda1ac4239b42016d47e0bc4c6621b9861
LONG <2447417474@qq.com>
2022-09-06 11:32:19 +0800

    gai

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>heima</div>
+  <div>h</div>
 </template>
 
 <script>
-
+// git log 查看commit记录
+// git status 查看当前仓库的状态
+// git add . 提交到暂存区
+// git commit -m “” （双引号里写这次存档的说明，存了啥）
+//    - feat：新增特性
+//    - style：样式的修改
+//    - fixed：debug
+//    - comment：添加了注释
 export default {}
 </script>
 
