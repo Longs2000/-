c43e4671361b89a57a0e50a2df0d24b08edb8cac
LONG <2447417474@qq.com>
2022-09-06 16:13:42 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>你好黑马</div>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
+  </div>
 </template>
 
 <script>
-// git log 查看commit记录
+// git log 查看commit记录cls
 // git status 查看当前仓库的状态
 // git add . 提交到暂存区
 // git commit -m “” （双引号里写这次存档的说明，存了啥）
@@ -14,4 +21,10 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+  .box {
+    width: 2rem;
+    height: 2rem;
+    background-color: gold;
+  }
+</style>
