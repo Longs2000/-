a436cdffa3ca89b2a4f9e4a9fa663df650e8a6aa
LONG <2447417474@qq.com>
2022-09-06 11:21:39 +0800

    初始化配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭eslint检查
+  lintOnSave: false,
+  // 自动的打开浏览器，端口指定(port)，域名指定(host)
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
